efe0149847fd650224e8a8cfc4c2c37519d23ca6
Royee <save95@126.com>
2017-05-17 20:03:34 +0800

    自动部署推广页面脚本： 1）支持使用配置文件配置各参数 2）优化 nginx 配置文件

--- websitedeploy/src/batch-deploy.sh
+++ websitedeploy/src/batch-deploy.sh
@@ -1,57 +1,56 @@
 #!/bin/sh
 
-# need edit
-readonly MS_FILE_PATH="."
-readonly NGINX_PATH="/usr/local/webserver/nginx"
-readonly WEB_CONF_RELPATH="conf/sites/webs"
-readonly WEB_ROOT_PATH="/home/web/wwwroot"
-readonly WEB_DIR_USER_GROUP="web:www"
+readonly NGINX_BIN=`sed '/^nginx_bin_dir\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
+readonly WEB_CONF_DIR=`sed '/^web_nginx_conf_dir\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
+readonly WEB_ROOT_DIR=`sed '/^web_root_dir\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
+readonly WEB_USER=`sed '/^web_user\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
+readonly WEB_USER_GROUP=`sed '/^web_user_group\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
 
-# nginx setting
-readonly MS_FILE="$MS_FILE_PATH/web.conf.ms"
-readonly NGINX_BIN="$NGINX_PATH/sbin"
-readonly NGINX_CONF_PATH="$NGINX_PATH/$WEB_CONF_RELPATH"
+# todo check params
+
+# const
+readonly MS_FILE='web.conf.ms'
 readonly SERVER_NAME_PATTERN="^([a-z0-9]+)((.[-a-z0-9]+)+)$"
 
-readonly LOCK_FILE="run.lock"
-readonly VALID_TMP_FILE="valid.tmp"
-readonly SUCCESS_LOG_FILE="success.log"
-readonly FAILD_LOG_FILE="faild.log"
-DATE=`date '+%F %T'`
-BACKUP_EXT=`date '+%F_%T'`
+readonly LOCK_FILE='.lock'
+readonly VALID_TMP_FILE='valid.tmp'
+readonly SUCCESS_LOG_FILE='success.log'
+readonly FAILD_LOG_FILE='faild.log'
+readonly DATE=`date '+%F %T'`
+readonly BACKUP_EXT=`date '+%F_%T'`
 
 # check lock
-if [ -f $LOCK_FILE ]; then
-    echo -e "\033[31m[Error]\033[0m The program is being used."
+if [ -f ${LOCK_FILE} ]; then
+    printf "\E[31m[Error]\E[0m The program is being used.\n"
     exit;
 fi
 
-touch $LOCK_FILE
+touch ${LOCK_FILE}
 
 
 function quitRun() {
-    rm -f $VALID_TMP_FILE
-    rm -f $LOCK_FILE
+    rm -f ${VALID_TMP_FILE}
+    rm -f ${LOCK_FILE}
     
-    if [ -f $SUCCESS_LOG_FILE ]; then
-        mv -f $SUCCESS_LOG_FILE "$SUCCESS_LOG_FILE.$BACKUP_EXT"
+    if [ -f ${SUCCESS_LOG_FILE} ]; then
+        mv -f ${SUCCESS_LOG_FILE} "${SUCCESS_LOG_FILE}.${BACKUP_EXT}"
     fi
-    if [ -f $FAILD_LOG_FILE ]; then
-        mv -f $FAILD_LOG_FILE "$FAILD_LOG_FILE.$BACKUP_EXT"
+    if [ -f ${FAILD_LOG_FILE} ]; then
+        mv -f ${FAILD_LOG_FILE} "${FAILD_LOG_FILE}.${BACKUP_EXT}"
     fi
     exit;
 }
 
 function check() {
-    vaild=1
-    serverName=$1
-    rootDir=$2
-    lineStr="$serverName $rootDir"
-    
-    printf "%-30s%-38s" "$serverName" "WEB_ROOT_PATH/$rootDir"
-    
+    local vaild=1
+    local serverName=$1
+    local rootDir=$2
+    local lineStr="${serverName} ${rootDir}"
+
+    printf "%-30s%-38s" "${serverName}" "WEB_ROOT_PATH/${rootDir}"
+
     # check domain
-    if [[ "$serverName" =~ $SERVER_NAME_PATTERN ]]; then
+    if [[ "${serverName}" =~ ${SERVER_NAME_PATTERN} ]]; then
         printf "\E[32m%-12s\E[0m" "[success]"
     else
         printf "\E[31m%-12s\E[0m" "[invalid]"
@@ -59,8 +58,8 @@ function check() {
     fi
 
     # check web dir
-    WEB_ROOT_DIR="$WEB_ROOT_PATH/$rootDir"
-    if [ ! -d "$WEB_ROOT_DIR" ]; then
+    local SITE_ROOT_DIR="${WEB_ROOT_DIR}/${rootDir}"
+    if [ ! -d "${SITE_ROOT_DIR}" ]; then
         printf "\E[32m%-12s\E[0m" "[success]"
     else
         printf "\E[31m%-12s\E[0m" "[existed]"
@@ -69,34 +68,36 @@ function check() {
 
 
     # check the nginx .conf file"
-    CONF_FILE="$NGINX_CONF_PATH/$serverName.conf"
-    if [ ! -f "$CONF_FILE" ]; then
-        printf "\E[32m%-12s\E[0m\n" "[success]"
-        if [ $vaild == 1 ]; then
-            echo $lineStr >> $VALID_TMP_FILE
+    local CONF_FILE="${WEB_CONF_DIR}/${serverName}.conf"
+    if [ ! -f "${CONF_FILE}" ]; then
+        printf "\E[32m%-12s\E[0m" "[success]"
+        if [ ${vaild} == 1 ]; then
+            echo ${lineStr} >> ${VALID_TMP_FILE}
         fi
     else
-        printf "\E[31m%-12s\E[0m\n" "[existed]"
+        printf "\E[31m%-12s\E[0m" "[existed]"
     fi
+
+    printf "\n"
 }
 
 function setWeb() {
-    ENTER_NEXT=0
-    serverName=$1
-    rootDir=$2
-    lineStr="$serverName $rootDir"
-    
-    printf "%-30s%-38s" "$serverName" "WEB_ROOT_PATH/$rootDir"
-    
+    local ENTER_NEXT=0
+    local serverName=$1
+    local rootDir=$2
+    local lineStr="${serverName} ${rootDir}"
+
+    printf "%-30s%-38s" "${serverName}" "WEB_ROOT_PATH/${rootDir}"
+
     # make dir
-    WEB_ROOT_DIR="$WEB_ROOT_PATH/$rootDir"
-    if [ ! -d "$WEB_ROOT_DIR" ]; then
-        mkdir -p $WEB_ROOT_DIR > /dev/null 2>&1
-        chown -R $WEB_DIR_USER_GROUP $WEB_ROOT_DIR > /dev/null 2>&1
+    local SITE_ROOT_DIR="${WEB_ROOT_DIR}/${rootDir}"
+    if [ ! -d "${SITE_ROOT_DIR}" ]; then
+        mkdir -p ${SITE_ROOT_DIR} > /dev/null 2>&1
+        chown -R ${WEB_USER}.${WEB_USER_GROUP} ${SITE_ROOT_DIR} > /dev/null 2>&1
 
         if [ "$?" == 1 ]; then
             printf "\E[31m%-12s%-12s\E[0m\n" "[faild]" "[skip]"
-            echo $lineStr >> $FAILD_LOG_FILE
+            echo ${lineStr} >> ${FAILD_LOG_FILE}
             ENTER_NEXT=0
         else
             printf "\E[32m%-12s\E[0m" "[success]"
@@ -106,19 +107,19 @@ function setWeb() {
         printf "\E[32m%-12s\E[0m" "[existed]"
         ENTER_NEXT=1
     fi
-    
+
     # make .conf file
-    if [ "$ENTER_NEXT" == 1 ]; then
-        CONF_FILE="$NGINX_CONF_PATH/$serverName.conf"
-        if [ ! -f "$CONF_FILE" ]; then
-            sed "s|SERVER_NAME|$serverName|g; s|ROOT_DIR|$rootDir|g" $MS_FILE > $serverName.conf.tmp
-            mv $serverName.conf.tmp $CONF_FILE > /dev/null 2>&1
+    if [ "${ENTER_NEXT}" == 1 ]; then
+        local CONF_FILE="${WEB_CONF_DIR}/${serverName}.conf"
+        if [ ! -f "${CONF_FILE}" ]; then
+            sed "s|SERVER_NAME|${serverName}|g; s|ROOT_DIR|${rootDir}|g" ${MS_FILE} > ${serverName}.conf.tmp
+            mv ${serverName}.conf.tmp ${CONF_FILE} > /dev/null 2>&1
             if [ "$?" == 1 ]; then
                 printf "\E[31m%-12s\E[0m\n" "[faild]"
-                echo $lineStr >> $FAILD_LOG_FILE
+                echo ${lineStr} >> ${FAILD_LOG_FILE}
             else
                 printf "\E[32m%-12s\E[0m\n" "[success]"
-                echo $lineStr >> $SUCCESS_LOG_FILE
+                echo ${lineStr} >> ${SUCCESS_LOG_FILE}
             fi
         else
             printf "\E[31m%-12s\E[0m" "[existed]"
@@ -129,94 +130,89 @@ function setWeb() {
 }
 
 function statistics() {
-    LINE_NUM=`cat $1|wc -l`
-    VALID_NUM=0
-    SUCCESS_NUM=0
-    FAILD_NUM=0
+    local LINE_NUM=`cat $1|wc -l`
+    local VALID_NUM=0
+    local SUCCESS_NUM=0
+    local FAILD_NUM=0
 
-    if [ -f $VALID_TMP_FILE ]; then
-        VALID_NUM=`cat $VALID_TMP_FILE|wc -l`
+    if [ -f ${VALID_TMP_FILE} ]; then
+        VALID_NUM=`cat ${VALID_TMP_FILE}|wc -l`
     fi
 
-    if [ -f $SUCCESS_LOG_FILE ]; then
-        SUCCESS_NUM=`cat $SUCCESS_LOG_FILE|wc -l`
+    if [ -f ${SUCCESS_LOG_FILE} ]; then
+        SUCCESS_NUM=`cat ${SUCCESS_LOG_FILE}|wc -l`
     fi
 
-    if [ -f $FAILD_LOG_FILE ]; then
-        FAILD_NUM=`cat $FAILD_LOG_FILE|wc -l`
+    if [ -f ${FAILD_LOG_FILE} ]; then
+        FAILD_NUM=`cat ${FAILD_LOG_FILE}|wc -l`
     fi
     
     printf "\E[1m%-12s%-12s%-12s%-12s\E[0m\n" TOTAL VALID SUCCESS FAILD
-    printf "%-12s%-12s%-12s%-12s%-12s\n" $LINE_NUM $VALID_NUM $SUCCESS_NUM $FAILD_NUM
+    printf "%-12s%-12s%-12s%-12s%-12s\n" ${LINE_NUM} ${VALID_NUM} ${SUCCESS_NUM} ${FAILD_NUM}
 }
 
 #
-if [ ! -f $MS_FILE ]; then
-    echo -e "\033[31m[Error]\033[0m MS file was not found."
+if [ ! -f ${MS_FILE} ]; then
+    printf "\E[31m[Error]\E[0m MS file was not found.\n"
     quitRun
 fi
 
 # input
 if [ "$#" != 1 ]; then
-    echo -e "\033[31m[Error]\033[0m Please enter the specified file."
-    echo -e "The file format for: \033[1mDOMAIN WEB_ROOT_DIR\033[0m"
-    echo "WEB_ROOT_DIR is relative to the path of the $WEB_ROOT_PATH/"
+    printf "\E[31m[Error]\E[0m Please enter the specified file.\n"
+    printf "\nNotice: The line format for: \n"
+    printf "\E[1mDOMAIN WEB_ROOT_DIR\E[0m\n"
+    printf "\nWEB_ROOT_DIR is relative to the path of the ${WEB_ROOT_DIR}/\n"
     quitRun
 fi
 
 readonly INPUT_FILE=$1
 
-echo ""
-echo -n "1) Checking the legitimacy of the file data..."
-echo ""
+printf "\n1) Checking the legitimacy of the file data...\n"
 printf "\E[1m%-30s%-38s%-12s%-12s%-12s\E[0m\n" DOMAIN WEB_DIR CK_DOMAIN CK_WEBDIR CK_CONFILE
-cat $INPUT_FILE | while read line
+cat ${INPUT_FILE} | while read line
 do
-    check $line
+    check ${line}
 done
 
 # check valid file
-if [ ! -f $VALID_TMP_FILE ]; then
-    echo "";
-    echo -e "\033[33m[Cancel]\033[0m No legal data, the system automatically terminate."
+if [ ! -f ${VALID_TMP_FILE} ]; then
+    printf "\n\E[33m[Cancel]\E[0m No legal data, the system automatically terminate.\n"
     quitRun
 fi
 
 # confirm
-echo ""
-echo -en "2) Will automatically configure legal data, please confirm whether to enter \033[1mYes\033[0m or other: "
+printf "\n2) Will automatically configure legal data, please confirm whether to enter \E[1mYes\E[0m or other: "
 read confirm
 if [ "$confirm" != "Yes" ]; then
-    echo -e "\033[33m[Cancel]\033[0m The user to cancel the operation."
+    printf "\n\E[33m[Cancel]\E[0m The user to cancel the operation.\n"
     quitRun
 fi
 
-echo ""
-echo "    2.1) The batch configuration in the website..."
-printf "\E[1m%-30s%-30s%-12s%-12s%-12s\E[0m\n" DOMAIN WEB_DIR MAKE_DIR COPY_CONF
-cat $VALID_TMP_FILE | while read line
+printf "\n2.1) The batch configuration in the website...\n"
+printf "\E[1m%-30s%-38s%-12s%-12s%-12s\E[0m\n" DOMAIN WEB_DIR MAKE_DIR COPY_CONF
+cat ${VALID_TMP_FILE} | while read line
 do
-    setWeb $line
+    setWeb ${line}
 done
 
 
-echo ""
-echo "    2.2) Automatic configuration result statistics:"
-statistics $INPUT_FILE
+printf "\n2.2) Automatic configuration result statistics:\n"
+statistics ${INPUT_FILE}
 
-echo ""
-echo "    2.3) Nginx configuration testing: "
-$NGINX_BIN/nginx -t
+printf "\n2.3) Nginx configuration testing: \n"
+${NGINX_BIN}/nginx -t
 
-echo ""
-echo -en "3) Please enter \033[1mYes\033[0m or other to confirm reload NGINX configuration: "
+printf "\n3) Please enter \E[1mYes\E[0m or other to confirm reload NGINX configuration: "
 read reloadNginx
 if [ "$reloadNginx" == "Yes" ]; then
-    echo -n "Overloading nginx configuration ................... "
-    $NGINX_BIN/nginx -s reload && echo -e "\033[32m[success]\033[0m" || echo echo -e "\033[31m[faild]\033[0m"
+    printf "\nOverloading nginx configuration ................... "
+    ${NGINX_BIN}/nginx -s reload && printf "\E[32m[success]\E[0m" || printf "\E[31m[faild]\E[0m"
+    printf "\n"
 else
-    echo -e "\033[33m[Warning]\033[0m Configuration has been completed, but not overloading nginx. Please manual operation: "
-    echo -e "Please Use:\033[1m $NGINX_BIN/nginx -s reload \033[0m"
+    printf "\E[33m[Warning]\E[0m Configuration has been completed, but not overloading nginx."
+    printf "Please Use:\E[1m ${NGINX_BIN}/nginx -s reload \E[0m"
+    printf "\n"
 fi
 
 quitRun
--- websitedeploy/src/deploy.sh
+++ websitedeploy/src/deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# need edit
-readonly NGINX_PATH="/usr/local/webserver/nginx"
-readonly WEB_CONF_RELPATH="conf/sites/webs"
-readonly WEB_ROOT_PATH="/home/web/wwwroot"
+readonly NGINX_BIN=`sed '/^nginx_bin_dir\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
+readonly WEB_CONF_DIR=`sed '/^web_nginx_conf_dir\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
+readonly WEB_ROOT_DIR=`sed '/^web_root_dir\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
+readonly WEB_USER=`sed '/^web_user\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
+readonly WEB_USER_GROUP=`sed '/^web_user_group\s*=.*$/!d;s/.*=\s*//;s/\s//g' config.ini`
 
-# nginx setting
-readonly NGINX_BIN="${NGINX_PATH}/sbin"
+# todo check params
+
+# const
 readonly SERVER_NAME_PATTERN="^([a-z0-9]+)((.[-a-z0-9]+)+)$"
-DATE=`date '+%F %T'`
+readonly DATE=`date '+%F %T'`
 
 # input
 if [ $# != 2 ]; then
@@ -18,7 +20,7 @@ fi
 
 if [ -z $1 -a -z $2 ]; then
     read -p "server name: " serverName
-    read -p "web root directory: ${WEB_ROOT_PATH}/" rootDir
+    read -p "web root directory: ${WEB_ROOT_DIR}/" rootDir
 elif [ -n $1 -a -n $2 ]; then
     serverName=$1
     rootDir=$2
@@ -31,54 +33,56 @@ readonly rootDir
 echo ""
 echo -n "Check the domain name ............. "
 if [[ "${serverName}" =~ ${SERVER_NAME_PATTERN} ]]; then
-    echo -e "\033[32m[success]\033[0m"
+    echo -e "\E[32m[success]\E[0m"
 else
-    echo -e "\033[31m[invalid]\033[0m"
+    echo -e "\E[31m[invalid]\E[0m"
     exit;
 fi
 
 echo -n "Check the web root directory ...... "
-readonly WEB_ROOT_DIR="${WEB_ROOT_PATH}/${rootDir}"
-if [ ! -d "${WEB_ROOT_DIR}" ]; then
-    echo -e "\033[32m[success]\033[0m"
+readonly SITE_ROOT_DIR="${WEB_ROOT_DIR}/${rootDir}"
+if [ ! -d "${SITE_ROOT_DIR}" ]; then
+    echo -e "\E[32m[success]\E[0m"
 else
-    echo -e "\033[31m[exist]\033[0m"
-#    exit;
+    echo -e "\E[31m[exist]\E[0m"
 fi
 
 echo -n "Check the nginx .conf file ........ "
-readonly CONF_FILE="${NGINX_PATH}/${WEB_CONF_RELPATH}/${serverName}.conf"
+readonly CONF_FILE="${WEB_CONF_DIR}/${serverName}.conf"
 if [ ! -f "${CONF_FILE}" ]; then
-    echo -e "\033[32m[success]\033[0m"
+    echo -e "\E[32m[success]\E[0m"
 else
-    echo -e "\033[31m[exist]\033[0m"
+    echo -e "\E[31m[exist]\E[0m"
     exit;
 fi
 
 # confirm
-echo ""
-echo -e "Please enter \033[1mYes\033[0m or other to confirm:"
-echo -e "\033[1m  server name :\033[0m ${serverName}"
-echo -e "\033[1m  web root dir:\033[0m ${WEB_ROOT_PATH}/${rootDir}"
-read -p "Please Confirm: " confirm
+printf "
+Please refer to the following information is correct:
+    \E[1m  server name :\E[0m ${serverName}
+    \E[1m  web root dir:\E[0m ${SITE_ROOT_DIR}
+    Please enter \E[1mYes\E[0m or other to confirm: "
+read confirm
 
 if [ ${confirm} != "Yes" ]; then
-    echo -e "\033[33m[Cancel]\033[0m The user to cancel the operation."
+    echo -e "\E[33m[Cancel]\E[0m The user to cancel the operation."
     exit;
 fi
 
 # make web root dir
 echo -n "Create the web root directory ..................... "
-if [ ! -d "${WEB_ROOT_DIR}" ]; then
-    mkdir -p ${WEB_ROOT_PATH}/${rootDir} > /dev/null 2>&1
-    chown -R web:www ${WEB_ROOT_PATH}/${rootDir} > /dev/null 2>&1
+if [ ! -d "${SITE_ROOT_DIR}" ]; then
+    mkdir -p ${SITE_ROOT_DIR} > /dev/null 2>&1
+    chown -R ${WEB_USER}.${WEB_USER_GROUP} ${SITE_ROOT_DIR} > /dev/null 2>&1
 
     if [ $? == 1 ]; then
-        echo -e "\033[31m[faild]\033[0m"
+        echo -e "\E[31m[faild]\E[0m"
         exit;
     else
-        echo -e "\033[32m[success]\033[0m"
+        echo -e "\E[32m[success]\E[0m"
     fi
+else
+    echo -e "\E[33m[exist]\E[0m"
 fi
 
 # copy conf file.
@@ -86,18 +90,19 @@ echo -n "Generate a web site nginx configuration file ...... "
 sed "s|SERVER_NAME|${serverName}|g; s|ROOT_DIR|${rootDir}|g" web.conf.ms > ${serverName}.conf.tmp
 mv ${serverName}.conf.tmp ${CONF_FILE} > /dev/null 2>&1
 if [ $? == 1 ]; then
-    echo -e "\033[31m[faild]\033[0m"
+    echo -e "\E[31m[faild]\E[0m"
     exit;
 else
-    echo -e "\033[32m[success]\033[0m"
+    echo -e "\E[32m[success]\E[0m"
 fi
 
 # write log
 echo "${DATE} set ${serverName}:" >> run.log
-echo "    file: ${NGINX_PATH}/${WEB_CONF_RELPATH}/${serverName}.conf" >> run.log
-echo "    dir : ${WEB_ROOT_PATH}/${rootDir}" >> run.log
+echo "    file: ${WEB_CONF_DIR}/${serverName}.conf" >> run.log
+echo "    dir : ${SITE_ROOT_DIR}" >> run.log
 echo "" >> run.log
 
+echo ""
 echo "Nginx configuration testing: "
 ${NGINX_BIN}/nginx -t
 
@@ -105,10 +110,10 @@ echo ""
 read -p "Immediately overloaded nginx configuration? [Yes or any other]: " reloadNginx
 if [ ${reloadNginx} == "Yes" ]; then
     echo -n "Overloading nginx configuration ................... "
-    ${NGINX_BIN}/nginx -s reload && echo -e "\033[32m[success]\033[0m" || echo echo -e "\033[31m[faild]\033[0m"
+    ${NGINX_BIN}/nginx -s reload && echo -e "\E[32m[success]\E[0m" || echo echo -e "\E[31m[faild]\E[0m"
     exit;
 else
-    echo -e "\033[33m[Warning]\033[0m Configuration has been completed, but not overloading nginx. Please manual operation."
+    echo -e "\E[33m[Warning]\E[0m Configuration has been completed, but not overloading nginx. Please manual operation."
 fi
 
 echo ""
